97c5033eb8f2a56d505acd913933e79f8fb45e63
chrgn <me@maeko.ch>
2025-02-19 06:11:49 +0100

    meow

--- js/last-modified.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener('DOMContentLoaded', () => {
-    document.getElementById('policy-last-modified').textContent = new Date(document.lastModified).toLocaleDateString('en-US', {
-        year: 'numeric',
-        month: 'short',
-        day: 'numeric'
-    });
-});
